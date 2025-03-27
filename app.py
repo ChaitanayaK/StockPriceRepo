ae34f57d5f0475cde98b409234e79d579e54f03d
Chaitanaya Kusumakar <chaitanayakusumakar7551@gmail.com>
2025-03-27 15:29:08 +0530

    Added some tweaks for working

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ app = Flask(__name__)
 @app.route('/api/stockprice', methods=['GET'])
 def get_data():
     try:
-        symbol = request.args.get('name', None)  
+        symbol = request.args.get('symbol')  
+        symbol = str(symbol).capitalize()
         stock_symbol = f"{symbol}.NS"  
         stock = yf.Ticker(stock_symbol)
         last_price = stock.info["regularMarketPrice"]
         return jsonify({"stock": stock_symbol, "last_price": last_price}), 200
-    except:
-        return ({"error": "Invalid Stock Symbol/Unable to fetch data"}), 400
+    except Exception as e:
+        return ({"error": e}), 400
