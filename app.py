2186c319f3c5d8acd35b186933bafe24c97fc399
Chaitanaya Kusumakar <chaitanayakusumakar7551@gmail.com>
2025-03-27 15:08:50 +0530

    Added app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, request
+import yfinance as yf
+
+app = Flask(__name__)
+
+@app.route('/api/stockprice', methods=['GET'])
+def get_data():
+    try:
+        symbol = request.args.get('name', None)  
+        stock_symbol = f"{symbol}.NS"  
+        stock = yf.Ticker(stock_symbol)
+        last_price = stock.info["regularMarketPrice"]
+        return jsonify({"stock": stock_symbol, "last_price": last_price}), 200
+    except:
+        return ({"error": "Invalid Stock Symbol/Unable to fetch data"}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
